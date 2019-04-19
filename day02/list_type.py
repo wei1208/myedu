2674b49c5ec0fc2a4ebadc968d28ef7119ea0dcd
吴学伟 <2224153253@qq.com>
2019-04-19 18:06:56 +0800

    新增 list 类型

--- day02/list_type.py
+++ day02/list_type.py
@@ -1,12 +1,39 @@
-def list_sel(a):
-    print( a[2] )
-    print(a[-1])
-    print(a[2:5])
-    print(a[0:2])
-    print(a[:4])
-    print(a[3:])
 
+def list_sel(b):
+
+
+    # print(b[-1])
+    # print(b[2:5])
+    # print(b[0:2])
+    # print(b[:4])
+    # print(b[2:4])
+    # print(b[2:])
+      print(b[5:])
+def list_del():
+    alist = ['a', 4, 'nihao', '8', '你好', '哈哈', '伟']
+    alist.pop()
+    print(alist)
+    alist.pop(3)
+    print(alist)
+    blist = alist[:-3]
+def list_add():
+    alist = ['a', 4, 'nihao', '8', '你好', '哈哈', '伟']
+    alist.append('哈哈哈哈')
+    print(alist)
+    blist = [1,2,3,4]
+    alist.append(blist)
+    print(alist)
+    alist[0] = 5
+    print(alist)
+def list_update():
+    alist = ['a', 4, 'nihao', '8', '你好', '哈哈', '伟']
+    alist[3] = '伟'
+    print(alist)
+# 替换
 if __name__ == '__main__':
-    alist = ['a',4,'nihao','8','你好','哈哈']
-    list_sel(alist)
-    # print(list)
+    alist = ['a',4,'nihao','8','你好','哈哈','伟']
+    # list_del()
+    # list_add()
+    # list_update()
+    # 长度
+    print(len(alist))
